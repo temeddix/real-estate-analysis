424d6eea7f47c810eceb8337fe37d92bbd981b12
DongHyun Kim <temeddix@gmail.com>
2023-07-24 02:41:10 +0900

    Add state session example

---
+++ pages/sample_page.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+
+# the callback function for the button will add 1 to the
+# slider value up to 10
+def plus_one():
+    if st.session_state["slider"] < 10:
+        st.session_state.slider += 1
+    else:
+        pass
+    return
+
+
+# when creating the button, assign the name of your callback
+# function to the on_click parameter
+add_one = st.button("Add one to the slider", on_click=plus_one, key="add_one")
+
+# create the slider
+slide_val = st.slider("Pick a number", 0, 10, key="slider")
