6ec38ffebfbbde62c8896c6101d38983235a437c
DongHyun Kim <temeddix@gmail.com>
2023-08-02 08:15:22 +0900

    Change a test loop message

--- background_work/test.py
+++ background_work/test.py
@@ -4,6 +4,8 @@ import aiohttp
 
 
 async def do_http_request():
+    count = 1
     while True:
-        print("YAHOO")
+        print(f"YAHOO {count}")
+        count += 1
         await asyncio.sleep(10)
